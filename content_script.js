6fffe7e4839022936838b033a387a69974af0dba
Patrick Clancey <patrick.clancey@cognifide.com>
2015-07-03 12:13:37 +0100

    Refactor assets

--- content_script.js
+++
@@ -1,9 +0,0 @@
-// show CBT test panel
-if("showCBTbookmarklet" in window){
-    showCBTbookmarklet();
-} else {
-    var cbt_script=document.createElement('SCRIPT');
-    cbt_script.type='text/javascript';
-    cbt_script.src='https://crossbrowsertesting.com/cbt_bookmarklet.js.php?random='+ +new Date();
-    document.getElementsByTagName('head')[0].appendChild(cbt_script);
-}
